eabe6c544a171ffbe39a22e4ccaa603d4ac44157
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-01-03 23:48:38 +0300

    Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,7 +2,7 @@
  * Retrieves students in a given location.
  * @params Array of student objects - The list of students.
  * @param {String} city - The location.
- * @author Aly Mtsumi <https://github.com/Mtsumi>
+ * @author Hillary Oyaro <https://github.com/hillaryoyaro>
  * @returns list of students in the city passed
  */
 export default function getStudentsByLocation(students, city) {
