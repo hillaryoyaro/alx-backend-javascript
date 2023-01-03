fc8857f41fa7e7159a3046c5e6ef4da47a8b89c3
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-01-03 23:51:04 +0300

    Reduce Method

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 /**
  * Retrieves the sum of ids of a list of students.
  * @params array of students - The list of students.
- * @author Aly Mtsumi <https://github.com/Mtsumi>
+ * @author Hillary Oyaro <https://github.com/hillaryoyaro>
  * @returns {Number}
  */
 export default function getStudentIdsSum(students) {
