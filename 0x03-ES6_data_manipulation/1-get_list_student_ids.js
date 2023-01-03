324749e3358b1f3080cf443f89de22c2baa29d67
hillaryoyaro <hillaryoyaroh@gmail.com>
2023-01-03 23:46:23 +0300

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,7 @@
 /**
  * Retrieves ids from a list of students.
  * @params students - The list of students.
- * @author Aly Mtsumi <https://github.com/Mtsumi>
+ * @author Hillary Oyaro <https://github.com/hillaryoyaro>
  * @returns Student IDs in a new list
  */
 export default function getListStudentIds(students) {
